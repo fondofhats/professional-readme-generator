226b7966a516d16e3690a358b597e3b98897eb74
Michael Giddings <mikegiddings@gmail.com>
2020-10-03 21:18:52 -0400

    added user input questions

--- index.js
+++ index.js
@@ -1,15 +1,85 @@
-// array of questions for user
-const questions = [
+const fs = require("fs");
+const inquirer = require("inquirer");
 
-];
+// array of questions for user
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      message: "What is your github username?",
+      name: "accountName",
+    },
+    {
+      type: "checkbox",
+      name: "badges",
+      message: "What badges would you like to display? (Check all that apply)",
+      choices: [
+        "Language Count",
+        "Top Language",
+        "Code Size",
+        "Repo Size",
+        "Issues",
+        "Issues Closed",
+        "Release Version by Date",
+        "License",
+      ],
+    },
+    {
+      type: "input",
+      message: "What is your github email address?",
+      name: "accountEmail",
+    },
+    {
+      type: "input",
+      message: "What is the name of the project repository?",
+      name: "projectTitle",
+    },
+    {
+      type: "input",
+      message: "Desribe your project.",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "How do you install this project?",
+      name: "install",
+    },
+    {
+      type: "input",
+      message: "How do you use this project?",
+      name: "use",
+    },
+    {
+      type: "input",
+      message: "What license is used for this project?:",
+      name: "license",
+    },
+    {
+      type: "input",
+      message: "How is this project tested?",
+      name: "test",
+    },
+    {
+      type: "input",
+      message: "Lastly, how can someone contribute to this project?",
+      name: "contribute",
+    },
+    {
+        type: "confirm",
+        name: "image",
+        message: "Would you like to display your user image?",
+        default: false,
+      },
+  ]);
+};
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
 function init() {
-
+    promptUser()
+        .then(answers => console.log(answers));        
 }
 
 // function call to initialize program
